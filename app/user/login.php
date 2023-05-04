da6937aea44ddb72ba3c42b4621963688550aa88
berryes <therealkispetofi@gmail.com>
2023-05-04 08:32:10 +0200

    added ignore

--- app/user/login.php
+++ app/user/login.php
@@ -4,9 +4,6 @@
 
 require_once('connect.php');
 
-// Looing for .env at the root directory
-//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-//$dotenv->load();
 
 // Checking if the inputs are filled
 $fields = [
