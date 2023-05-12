756873e20a9a0de9c5d5678d7a28e6ad063d05c7
Dr. Zsíroskalács <129590056+KoromHusi@users.noreply.github.com>
2023-05-12 12:36:01 +0200

    Add files via upload

--- app/global/db/Bluementa.sql
+++ app/global/db/Bluementa.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 09, 2023 at 01:49 PM
--- Server version: 8.0.32-0ubuntu0.20.04.2
--- PHP Version: 7.4.3-4ubuntu2.18
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2023. Máj 12. 12:34
+-- Kiszolgáló verziója: 10.4.24-MariaDB
+-- PHP verzió: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,28 +18,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Bluementa`
+-- Adatbázis: `bluementa`
 --
-
-CREATE DATABASE Bluementa;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Clubs`
+-- Tábla szerkezet ehhez a táblához `clubs`
 --
 
-CREATE TABLE `Clubs` (
-  `club_id` int NOT NULL,
-  `club_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `club_description` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci NOT NULL
+CREATE TABLE `clubs` (
+  `club_id` int(11) NOT NULL,
+  `club_name` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `club_description` varchar(400) COLLATE utf8mb4_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- Dumping data for table `Clubs`
+-- A tábla adatainak kiíratása `clubs`
 --
 
-INSERT INTO `Clubs` (`club_id`, `club_name`, `club_description`) VALUES
+INSERT INTO `clubs` (`club_id`, `club_name`, `club_description`) VALUES
 (2, 'Valakiék', 'hoppá'),
 (3, 'dvsvdsv', ''),
 (6, 'Egy új Club', 'leírással');
@@ -48,26 +45,26 @@ INSERT INTO `Clubs` (`club_id`, `club_name`, `club_description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Questions`
+-- Tábla szerkezet ehhez a táblához `questions`
 --
 
-CREATE TABLE `Questions` (
-  `question_id` int NOT NULL,
-  `registration_id` int NOT NULL,
-  `quiz_id` int NOT NULL,
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL,
+  `registration_id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
   `question_question` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
   `question_answers` varchar(500) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `question_markable_answers` int NOT NULL,
+  `question_markable_answers` int(11) NOT NULL,
   `question_correct_answers` varchar(500) COLLATE utf8mb4_hungarian_ci NOT NULL,
   `question_description` varchar(500) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `question_type` int NOT NULL
+  `question_type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- Dumping data for table `Questions`
+-- A tábla adatainak kiíratása `questions`
 --
 
-INSERT INTO `Questions` (`question_id`, `registration_id`, `quiz_id`, `question_question`, `question_answers`, `question_markable_answers`, `question_correct_answers`, `question_description`, `question_type`) VALUES
+INSERT INTO `questions` (`question_id`, `registration_id`, `quiz_id`, `question_question`, `question_answers`, `question_markable_answers`, `question_correct_answers`, `question_description`, `question_type`) VALUES
 (1, 1, 1, 'Mennyi 2+2?', '4;7;11;19', 1, '4', 'Valami mas', 1),
 (2, 2, 1, 'Mennyi 5+5?', '12;21;10;2', 1, '1', 'Valami meg egy', 1),
 (3, 3, 2, 'Melyikek magyar szavak?', 'tapasz;ice;irodalom;ablak;picture', 3, 'tapasz;irodalom;ablak', 'Melyik magyar szo', 2),
@@ -80,40 +77,61 @@ INSERT INTO `Questions` (`question_id`, `registration_id`, `quiz_id`, `question_
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Quizzes`
+-- Tábla szerkezet ehhez a táblához `questions_quizzes_switch`
 --
 
-CREATE TABLE `Quizzes` (
-  `quiz_id` int NOT NULL,
-  `registration_id` int NOT NULL,
-  `group_id` int NOT NULL,
-  `quiz_name` varchar(125) CHARACTER SET utf8mb3 COLLATE utf8mb3_hungarian_ci NOT NULL,
-  `quiz_description` varchar(350) CHARACTER SET utf8mb3 COLLATE utf8mb3_hungarian_ci NOT NULL,
-  `number_of_questions` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
+CREATE TABLE `questions_quizzes_switch` (
+  `questions_quiz_id` int(11) NOT NULL,
+  `quizzes_quiz_id` int(11) NOT NULL,
+  `questions_quizzes_switch_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
--- Dumping data for table `Quizzes`
+-- A tábla adatainak kiíratása `questions_quizzes_switch`
 --
 
-INSERT INTO `Quizzes` (`quiz_id`, `registration_id`, `group_id`, `quiz_name`, `quiz_description`, `number_of_questions`) VALUES
-(3, 3, 3, 'ysdf', 'sdfa', 3),
-(121, 43, 34, '23123muk4fg', 'as', 312),
-(122, 12345, 1233, 'sdfasdfsd', 'fasdhgt', 54),
-(123, 3, 1234, 'fsdaf', '4sdf', 3123),
-(123123, 1231231, 23123, 'sdadfasdf', 'asdfasdf', 123),
-(123234, 234, 1223, 'fssfdafdsdaf', 'sadf', 23);
+INSERT INTO `questions_quizzes_switch` (`questions_quiz_id`, `quizzes_quiz_id`, `questions_quizzes_switch_id`) VALUES
+(3, 4, 1),
+(2, 4, 2),
+(2, 5, 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Users`
+-- Tábla szerkezet ehhez a táblához `quizzes`
 --
 
-CREATE TABLE `Users` (
-  `user_id` int NOT NULL,
-  `group_id` int NOT NULL,
-  `user_registration_id` int NOT NULL,
+CREATE TABLE `quizzes` (
+  `quiz_id` int(11) NOT NULL,
+  `registration_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `quiz_name` varchar(125) COLLATE utf8_hungarian_ci NOT NULL,
+  `quiz_description` varchar(350) COLLATE utf8_hungarian_ci NOT NULL,
+  `number_of_questions` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `quizzes`
+--
+
+INSERT INTO `quizzes` (`quiz_id`, `registration_id`, `group_id`, `quiz_name`, `quiz_description`, `number_of_questions`) VALUES
+(1, 43, 34, '23123muk4fg', 'as', 312),
+(2, 3, 3, 'ysdf', 'sdfa', 3),
+(3, 12345, 1233, 'sdfasdfsd', 'fasdhgt', 54),
+(4, 3, 1234, 'fsdaf', '4sdf', 3123),
+(5, 1231231, 23123, 'sdadfasdf', 'asdfasdf', 123),
+(6, 234, 1223, 'fssfdafdsdaf', 'sadf', 23);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `user_registration_id` int(11) NOT NULL,
   `user_name` text COLLATE utf8mb4_hungarian_ci NOT NULL,
   `user_pass` text COLLATE utf8mb4_hungarian_ci NOT NULL,
   `user_email` text COLLATE utf8mb4_hungarian_ci NOT NULL,
@@ -123,60 +141,86 @@ CREATE TABLE `Users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- Indexes for dumped tables
+-- Indexek a kiírt táblákhoz
 --
 
 --
--- Indexes for table `Clubs`
+-- A tábla indexei `clubs`
 --
-ALTER TABLE `Clubs`
+ALTER TABLE `clubs`
   ADD PRIMARY KEY (`club_id`);
 
 --
--- Indexes for table `Questions`
+-- A tábla indexei `questions`
 --
-ALTER TABLE `Questions`
-  ADD PRIMARY KEY (`question_id`);
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_id`),
+  ADD KEY `quiz_id` (`quiz_id`);
 
 --
--- Indexes for table `Quizzes`
+-- A tábla indexei `questions_quizzes_switch`
 --
-ALTER TABLE `Quizzes`
+ALTER TABLE `questions_quizzes_switch`
+  ADD PRIMARY KEY (`questions_quizzes_switch_id`),
+  ADD KEY `questions_quiz_id` (`questions_quiz_id`),
+  ADD KEY `quizzes_quiz_id` (`quizzes_quiz_id`) USING BTREE;
+
+--
+-- A tábla indexei `quizzes`
+--
+ALTER TABLE `quizzes`
   ADD PRIMARY KEY (`quiz_id`);
 
 --
--- Indexes for table `Users`
+-- A tábla indexei `users`
 --
-ALTER TABLE `Users`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- A kiírt táblák AUTO_INCREMENT értéke
 --
 
 --
--- AUTO_INCREMENT for table `Clubs`
+-- AUTO_INCREMENT a táblához `clubs`
 --
-ALTER TABLE `Clubs`
-  MODIFY `club_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `clubs`
+  MODIFY `club_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `Questions`
+-- AUTO_INCREMENT a táblához `questions`
 --
-ALTER TABLE `Questions`
-  MODIFY `question_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `questions`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `Quizzes`
+-- AUTO_INCREMENT a táblához `questions_quizzes_switch`
 --
-ALTER TABLE `Quizzes`
-  MODIFY `quiz_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123235;
+ALTER TABLE `questions_quizzes_switch`
+  MODIFY `questions_quizzes_switch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `Users`
+-- AUTO_INCREMENT a táblához `quizzes`
 --
-ALTER TABLE `Users`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `quizzes`
+  MODIFY `quiz_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123235;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `questions_quizzes_switch`
+--
+ALTER TABLE `questions_quizzes_switch`
+  ADD CONSTRAINT `questions_quizzes_switch_ibfk_1` FOREIGN KEY (`quizzes_quiz_id`) REFERENCES `quizzes` (`quiz_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `questions_quizzes_switch_ibfk_2` FOREIGN KEY (`questions_quiz_id`) REFERENCES `questions` (`quiz_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
