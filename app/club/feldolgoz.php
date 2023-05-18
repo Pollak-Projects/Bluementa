f31c3d41b0e5ae2c92d8754b973d08debdb7d3d0
IWillSettYou <114726553+IWillSettYou@users.noreply.github.com>
2023-05-18 09:39:57 +0200

    Updated, but no worky worky

--- app/club/feldolgoz.php
+++ app/club/feldolgoz.php
@@ -1,29 +1,40 @@
 <?php
-// This Code is made by Imii and Gabor 
+// This Code is made by Imii and Gabor and Barta uwu
 
-require_once("global_connect.php");
+require_once("connect.php");
+//Checking if the body is filled
+$fields = [
+    'club_id',
+    'csoport',
+];
+
+foreach( $fields as $field){
+    if(!$_POST[$field]){
+        echo 'missing field: ' . $field;
+        return http_response_code(400);
+    }
+}
+
 // Adatok lekérése
 $csoport = $_POST['csoport'];
-$description =  $_POST['des'];
+$description = $_POST['des'];
 //var_dump($description);
-if (!$description == NULL)
+
+if ( !isset($description) )
 {
     // Csapat készítés leírással együtt
-    $sql = "INSERT INTO `club` (`club_name`,`club_description`)
-    VALUES (?, ?)";
-   $stmt = $mysqli->prepare($sql);
+    $sql = "INSERT INTO clubs (club_name, club_description) VALUES (?, ?)";
+    $stmt = $mysqli->prepare($sql);
    
-
-   $stmt->bind_param("ss", $csoport,$description);
-   $stmt->execute();
-   echo "Sikeres beküldés";
+    $stmt->bind_param("ss", $csoport,$description);
+    $stmt->execute();
+    echo "Sikeres beküldés";
 
 }
 else
 {
     // Csapat készítés leírás nélkül
-    $sql = "INSERT INTO `club` (`club_name`)
-    VALUES (?)";
+    $sql = "INSERT INTO clubs (club_name) VALUES (?)";
    $stmt = $mysqli->prepare($sql);
    
 
@@ -32,4 +43,54 @@ else
    echo "Sikeres beküldés";
 
 }
+
+$stmt->close();
+$mysqli->next_result();
+
+$sql = "SELECT club_name FROM club";
+$row = array();
+if ($result = $mysqli -> query($sql)) {
+    $row = $result -> fetch_row();
+
+    $result -> free_result();
+}
+
+
+$mysqli->next_result();
+
+
+
+
+function generateToken() {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+ 
+    for ($i = 0; $i < 5; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+ 
+    return $randomString;
+}
+
+$token = generateToken();
+
+// lil' variable to keep gyula away uwu lol xD
+$idk = "Fixy wixy me pls owo o_O";          
+
+$sql = "INSERT INTO club_tokens (token, club_id, user_id) VALUES(?, ?, ?)";
+$stmt = $mysqli->prepare($sql);
+
+$stmt->bind_param("sis", generateToken(), $row[0], $idk);
+$stmt->execute();
+$stmt->store_result();
+
+if(mysqli_affected_rows($mysqli) <= 1){
+    return http_response_code(201);
+}
+else{
+    return http_response_code(409);
+}
+
+
 ?>
