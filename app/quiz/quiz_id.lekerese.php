e234cb1d3c887facee68ca811ddbf862cde66317
dancsikdavid <130552807+dancsikdavid@users.noreply.github.com>
2023-05-19 12:56:05 +0200

    Update quiz_id.lekerese.php

--- app/quiz/quiz_id.lekerese.php
+++ app/quiz/quiz_id.lekerese.php
@@ -1,6 +1,6 @@
 <?php
-require ('user_id.kerese.php');
+include ('user_id.lekerese.php');
 
-$id = "SELECT quiz_id FROM Quizzes WHERE registration_id = $_SESSION["id"]";
+$sql = "SELECT quiz_id FROM Questions WHERE registration_id = $_SESSION["id"]";
 
 ?>
